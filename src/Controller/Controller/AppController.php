a7b594e6219633857432d1e7a42035d2d2f1b85e
Kerrial Newham <41723430+Kerrialn@users.noreply.github.com>
2024-11-02 10:46:52 +0100

    Add BootstrapCompilerService | add ScssService | add CustomizerPage (#3)
    * Add BootstrapCompilerService | add ScssService | add CustomizerPage
    
    * remove docker files | add to gitignore

--- src/Controller/Controller/AppController.php
+++ src/Controller/Controller/AppController.php
@@ -2,39 +2,15 @@
 
 namespace App\Controller\Controller;
 
-use App\DataTransferObject\VariablesDto;
-use App\Form\Form\BootstrapGeneratorFormType;
-use App\Service\BootstrapCustomizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AppController extends AbstractController
 {
-
-
-    public function __construct(
-        private readonly BootstrapCustomizer $bootstrapCustomizer
-    )
-    {
-    }
-
     #[Route(path: '/', name: 'landing')]
-    public function landing(Request $request): Response
+    public function landing(): Response
     {
-        $variablesDto = new VariablesDto();
-        $bootstrapGeneratorForm = $this->createForm(type: BootstrapGeneratorFormType::class, data: $variablesDto);
-
-        $bootstrapGeneratorForm->handleRequest($request);
-        if ($bootstrapGeneratorForm->isSubmitted() && $bootstrapGeneratorForm->isValid()) {
-            $css = $this->bootstrapCustomizer->compileCustomBootstrap(variablesDto: $variablesDto);
-            dd($css);
-        }
-
-        return $this->render('app/landing.html.twig', [
-            'bootstrapGeneratorForm' => $bootstrapGeneratorForm,
-            'variablesDto' => $variablesDto
-        ]);
+        return $this->render('app/landing.html.twig');
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const Encore = require('@symfony/webpack-encore');
 const webpack = require('webpack');
+const path = require('path');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -32,6 +33,7 @@ Encore
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
+    .addEntry('custom-bootstrap', './assets/styles/custom-bootstrap.scss')
 
     /*
      * FEATURE CONFIG
