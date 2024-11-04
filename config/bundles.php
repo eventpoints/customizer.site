0ad5e297df921d31f64e1593721875ce71b32d30
Fabricio872 <fabricio872@gmail.com>
2024-11-04 15:23:50 +0100

    implemented CORS (#13)
    * implemented CORS
    
    * cs fix

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\Vue\VueBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
