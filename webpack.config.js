31ef2fc7a18bde7855e4573ea7590bebd1ed962c
kez <kerrialbeckettnewham@gmail.com>
2024-11-05 17:08:57 +0100

    remove unused entry point

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,6 @@ Encore
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
-    .addEntry('custom-bootstrap', './assets/styles/custom-bootstrap.scss')
 
     /*
      * FEATURE CONFIG
