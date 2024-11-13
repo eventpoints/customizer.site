03d2b4633fad0870353ad94babbfa88e59c0fa19
Fabricio872 <fabricio872@gmail.com>
2024-11-13 10:33:23 +0100

    removed dart sass compiler (#26)
    * removed dart sass compiler
    returned back php sass compiler
    
    * added better exception handler

--- src/Controller/Api/CompileController.php
+++ src/Controller/Api/CompileController.php
@@ -12,7 +12,6 @@ use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Cache\CacheInterface;
-use Throwable;
 
 #[OA\Tag(name: 'Compile')]
 #[Route(path: '/compile', methods: ['POST'])]
@@ -20,7 +19,7 @@ class CompileController extends AbstractController
 {
     public function __construct(
         private readonly BootstrapCompilerService $bootstrapCompilerService,
-        private readonly CacheInterface $cache,
+        private readonly CacheInterface           $cache,
     )
     {
     }
@@ -45,9 +44,8 @@ class CompileController extends AbstractController
     public function compileBootstrap53(#[MapRequestPayload(serializationContext: ['groups' => ['compile']])] CustomizerFormBootstrap53Dto $bootstrap53Dto, #[MapQueryParameter] bool $isCached = false): JsonResponse
     {
         $variables = ClassPropertyService::getClassProperties(rootDto: $bootstrap53Dto);
-
         if ($isCached) {
-            return $this->cache->get(sprintf('compiledBootstrap-%s', crc32(serialize($variables))), fn(): \Symfony\Component\HttpFoundation\JsonResponse => $this->getCss($variables));
+            return $this->cache->get(sprintf('compiledBootstrap-%s', crc32(serialize($variables))), fn(): JsonResponse => $this->getCss($variables));
         }
 
         return $this->getCss($variables);
@@ -56,15 +54,9 @@ class CompileController extends AbstractController
     /**
      * @param array<string, string> $variables
      */
-    private function getCss(array $variables):JsonResponse
+    private function getCss(array $variables): JsonResponse
     {
         $css = $this->bootstrapCompilerService->compileCustomBootstrap(variables: $variables);
-
-        if ($css instanceof Throwable) {
-            return new JsonResponse(data: [
-                'message' => $css->getMessage(),
-            ], status: 500);
-        }
         return new JsonResponse(data: $css);
     }
 }
